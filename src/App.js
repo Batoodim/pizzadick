a3c9988d8b4ec47302aa0bad8b5696cbdfd1b953
Batoodim <dima12235@gmail.com>
2022-07-15 11:46:22 +0300

    add redux in Home.jsx

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect} from 'react';
+import axios from 'axios';
+import { useDispatch} from 'react-redux';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Route, Routes } from 'react-router-dom';
+
+import { Header } from './components';
+import { Home, Cart } from "./pages";
+import { setPizzas } from "./redux/actions/pizzas";
+
+function App( ) {
+
+    const dispatch = useDispatch();
+
+    useEffect(()=> {
+        axios.get('http://localhost:3000/db.json').then(( { data } ) => {
+            dispatch(setPizzas(data.pizzas))
+        });
+    });
+
+    return(
+        <div className="wrapper">
+            <Header />
+            <div className="content">
+                <Routes>
+                    <Route path="/" element={<Home />}  />
+                    <Route path="/cart" element={<Cart />}  />
+                </Routes>
+            </div>
+          </div>
+    );
 }
+
+// const mapStateToProps = state => {
+//     return {
+//         items: state.pizzas.items,
+//         filters: state.filters,
+//     }
+// };
+
+// const mapDispatchToProps = dispatch => {
+//     return{
+//         setPizzas: (items) => dispatch(setPizzas(items)),
+//     }
+// }
 
 export default App;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,45 @@
+import React, {useCallback} from 'react';
+import { Categories, SortPopup, PizzaBlock} from '../components';
+import {useSelector, useDispatch} from 'react-redux';
+
+import {setCategory} from'../redux/actions/filters';
+
+const categoryNames = ['Мясные','Вегетарианская','Гриль','Острые','Закрытые'];
+const sortItems = [
+    {name: 'популярности', type: 'popular'},
+    {name:'цена', type:'price'},
+    {name:'алфавиту', type:'alphabet'}
+];
+
+
+const Home = () => {
+    const dispatch = useDispatch();
+    const items = useSelector(({pizzas}) =>  pizzas.items);
+
+    const onSelectCategory = useCallback((index) => {
+        dispatch(setCategory(index));
+    }, []);
+
+    return (
+        <div className="container">
+                  <div className="content__top">
+
+                      <Categories
+                          onClickItem = {onSelectCategory}
+                          items={categoryNames}/>
+
+                      <SortPopup items={sortItems}/>
+                  </div>
+                  <h2 className="content__title">Все пиццы</h2>
+                  <div className="content__items">
+                      {
+                           items &&  items.map((obj) =>
+                                  <PizzaBlock key={obj.id} {...obj}/>
+                              )
+                      }
+                  </div>
+              </div>
+    );
+};
+
+export default Home;
